cddb1a0b9a6c64e86e91335d3230be4b28db0480
Gilbert Pellegrom <drummermanny@gmail.com>
2012-06-26 14:19:49 +0100

    Add plugin path/url vars.

--- wp-plugin-boilerplate.php
+++ wp-plugin-boilerplate.php
@@ -12,30 +12,37 @@ License: GPL2
 
 class WordPressPluginBoilerplate {
 
+    private $plugin_path;
+    private $plugin_url;
+
     function __construct() 
     {	
-        // Set up some defaults
+        // Set up default vars
+        $this->plugin_path = plugin_dir_path( __FILE__ );
+        $this->plugin_url = plugin_dir_url( __FILE__ );
+        // Set up activation hooks
         register_activation_hook( __FILE__, array(&$this, 'activate') );
         register_deactivation_hook( __FILE__, array(&$this, 'deactivate') );
+        // Set up l10n
         load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
         
-        // Use the constructor to add your own hooks/filters
+        // Add your own hooks/filters
         add_action( 'init', array(&$this, 'init') );
     }
     
     function activate( $network_wide ) 
     {
-    
+        
     }
     
     function deactivate( $network_wide ) 
     {
-    
+        
     }
     
     function init()
     {
-    
+        
     }
 
 }
