56aa308dca1c95b31dfb94dfb4757c40cf7af83a
Gilbert Pellegrom <drummermanny@gmail.com>
2012-06-26 13:54:07 +0100

    v1.0

--- wp-plugin-boilerplate.php
+++ wp-plugin-boilerplate.php
@@ -12,30 +12,30 @@ License: GPL2
 
 class WordPressPluginBoilerplate {
 
-    function __construct() 
-    {	
-    	// Set up some defaults
-    	register_activation_hook( __FILE__, array(&$this, 'activate') );
+	function __construct() 
+	{	
+		// Set up some defaults
+		register_activation_hook( __FILE__, array(&$this, 'activate') );
 		register_deactivation_hook( __FILE__, array(&$this, 'deactivate') );
-    	load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
-    	
-        // Use the constructor to add your own hooks/filters
-        add_action( 'init', array(&$this, 'init') );
+		load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+		
+		// Use the constructor to add your own hooks/filters
+		add_action( 'init', array(&$this, 'init') );
 	}
 	
 	function activate( $network_wide ) 
 	{
-		
+	
 	}
 	
 	function deactivate( $network_wide ) 
 	{
-		
+	
 	}
 	
 	function init()
 	{
-		
+	
 	}
 
 }
