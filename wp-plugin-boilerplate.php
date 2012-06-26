a36b654a5c2909f9418f3a87459535f4ed6097dc
Gilbert Pellegrom <drummermanny@gmail.com>
2012-06-26 13:56:20 +0100

    Converting to soft tabs.

--- wp-plugin-boilerplate.php
+++ wp-plugin-boilerplate.php
@@ -12,31 +12,31 @@ License: GPL2
 
 class WordPressPluginBoilerplate {
 
-	function __construct() 
-	{	
-		// Set up some defaults
-		register_activation_hook( __FILE__, array(&$this, 'activate') );
-		register_deactivation_hook( __FILE__, array(&$this, 'deactivate') );
-		load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
-		
-		// Use the constructor to add your own hooks/filters
-		add_action( 'init', array(&$this, 'init') );
-	}
-	
-	function activate( $network_wide ) 
-	{
-	
-	}
-	
-	function deactivate( $network_wide ) 
-	{
-	
-	}
-	
-	function init()
-	{
-	
-	}
+    function __construct() 
+    {	
+        // Set up some defaults
+        register_activation_hook( __FILE__, array(&$this, 'activate') );
+        register_deactivation_hook( __FILE__, array(&$this, 'deactivate') );
+        load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+        
+        // Use the constructor to add your own hooks/filters
+        add_action( 'init', array(&$this, 'init') );
+    }
+    
+    function activate( $network_wide ) 
+    {
+    
+    }
+    
+    function deactivate( $network_wide ) 
+    {
+    
+    }
+    
+    function init()
+    {
+    
+    }
 
 }
 new WordPressPluginBoilerplate();
